19cebe7d3049d03d15db2c4b6f54b4317d38e923
SilentSammy <scabrera197@gmail.com>
2026-05-22 18:21:13 -0600

    d6: manual game_detector setup

--- d6_plot.py
+++ d6_plot.py
@@ -1,8 +1,20 @@
 import time
+import cv2
 
 from cam_config import global_cam
-from game_det import game_detector
+from game_det import GameDetector
+from board_est import BoardEstimator
+from board_config import global_board_config
+from obj_det import BallDetector, ArucoDetector
 from zmq_comms import PlotPublisher
+
+game_detector = GameDetector(
+    board_estimator=BoardEstimator(global_board_config, K=global_cam.K, D=global_cam.D, rotate_180=True),
+    ball_detector=BallDetector(),
+    ball_height=0.02,
+    aruco_detector=ArucoDetector(cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)),
+    player_height=0.1,
+)
 
 with PlotPublisher() as plotter:
     try:
